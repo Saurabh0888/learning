bac329c338f94f0f55ed87e309cd753de1768655
Jaiswal <saurabh.jaiswal@xlgroup.com>
2020-04-26 20:26:14 +0530

    This is my first commit

---
+++ my_calender.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+
+def is_weekday():
+    today = datetime.today()
+    return (0<= today.weekday()<5)
+
+# def get_holidays():
+#     r = requests.get('http://localhost/api/holidays')
+#     if r.status_code == 200:
+#         return r.json()
+#     return None
+
+
+class Holiday():
+    def get_holidays(self):
+        r = requests.get('http://localhost/api/holidays')
+        if r.status_code == 200:
+            return r.json()
+        return None
